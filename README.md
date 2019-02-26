d01b881ff71bf9e8aacb37ff14eaec8a838e2eb4
dev <dev@decent.bet>
2019-02-26 05:28:23 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
-# node-mpp
-node library to perform multiparty protocol (MPP) functionality for contracts on the Vechain Thor blockchain
+# Node MPP
+
+Simple node library to setup Vechain's [MPP](https://github.com/vechain/thor-wiki/blob/5dbc2d8a287c4158b556ee8ec681243d0ccc89a1/Prototype(EN).md) for any deployed contract on a compatible Vechain Thor Blockchain.
+
+## Pre-requisites
+
+* [npm](https://npmjs.com)
+* [Vechain Thor](https://github.com/vechain/thor)
+
+## Setting up
+
+1. [Install npm](https://www.npmjs.com/get-npm)
+   
+1. Install [Vechain Thor](https://github.com/vechain/thor)
+
+1. Clone this repository
+
+1. Install the repository packages
+
+    ```
+    npm install
+    ```
+
+## Instructions
+
+Initialize a new node MPP instance like below
+
+```
+const mpp = new MPP(
+    contractAddress,
+    privateKey
+)
+```
+
+**Parameters**
+
+* contractAddress - Address of contract to perform MPP actions on
+* privateKey - Private key for contract address
+
+Note: A node MPP instance is designed to handle one contract address on initialization. 
+If you intend to handle multiple contract addresses, you would have to use multiple instances of node MPP.
+
+You can now use the mpp instance to perform any of commands below. Node MPP performs checks before
+sending state changing transactions to reduce transaction reverts and wasted energy.
+
+## Commands
+
+### Transactions
+
+#### sponsor()
+
+Volunteers to sponsor the contract using the default account linked to the MPP instance' private key.
+
+#### unsponsor()
+
+Removes the default account from being a sponsor of the contract if it had been a sponsor earlier.
+
+#### selectSponsor(sponsor)
+
+**Parameters**
+
+* sponsor - Address of sponsor to be selected
+
+Selects a sponsor address. The sponsor must have volunteered to be a sponsor by calling `sponsor()`. This can only be sent by the current contract master.
+
+#### selectMaster(master)
+
+**Parameters**
+
+* master - Address of master to be selected
+
+Selects a master address. This can only be sent by the current contract master.
+
+#### addUser(address)
+
+**Parameters**
+
+* address - Address of user to be added
+
+Adds a new user to the MPP whitelist for the contract. Throws if the user has already been added. This can only be sent by the current contract master.  
+
+#### removeUser(address)
+
+**Parameters**
+
+* address - Address of user to be removed
+
+Removes a user from the MPP whitelist of the contract. Throws if the user has not been added. This can only be sent by the current contract master.
+
+#### setCreditPlan(credit, recoveryRate)
+
+**Parameters**
+
+* credit - the maximum amount of VTHO (in wei) that can be accumulated
+* recoveryRate - amount of VTHO (in wei) accumulated per block to pay for transactions for each user
+
+Sets a new credit plan for the contract address as defined by the passed credit and recovery rate. This can only be sent by the current contract master.
+
+### Calls
+
+#### isSponsor(address)
+
+**Parameters**
+
+* address - Address to check sponsor status
+
+Returns whether an address has volunteered to sponsor the contract.
+
+#### isUser(address)
+
+**Parameters**
+
+* address - Address to check user status
+
+Returns whether an address has been added to the contract whitelist.
+
+#### currentSponsor()
+
+Returns the current selected sponsor for the contract.
+
+#### currentMaster()
+
+Returns the current master for the contract.
+
+#### getCreditPlan()
+
+Returns the active credit plan for the contract.
---
+++ index.js
@@ -0,0 +1,184 @@
+const mpp = require('./mpp')
+const Web3 = require('web3')
+const {thorify} = require('thorify')
+
+const web3 = new Web3()
+
+thorify(
+    web3,
+    'http://localhost:8669'
+)
+
+/**
+ * Node MPP instance
+ * @param contractAddress Address of contract
+ * @param privateKey Contract master private key
+ * @constructor
+ */
+function MPP (
+    contractAddress,
+    privateKey
+) {
+
+    // Init MPP contract
+    let mppContract = new web3.eth.Contract(
+        mpp.abi,
+        mpp.address
+    )
+
+    // Add private keys
+    web3.eth.accounts.wallet.add(privateKey)
+
+    // Default transaction options
+    const defaultTxOptions = {
+        from: web3.eth.accounts.wallet[0].address,
+        gas: 1000000
+    }
+
+    // Transactions
+    const {
+        addUser,
+        removeUser,
+        setCreditPlan,
+        selectSponsor,
+        selectMaster,
+        sponsor
+    } = require('./lib/contract/tx')(
+        contractAddress,
+        mppContract,
+        defaultTxOptions
+    )
+
+    // Calls
+    const {
+        creditPlan,
+        isUser,
+        isSponsor,
+        currentSponsor,
+        master
+    } = require('./lib/contract/call')(
+        contractAddress,
+        mppContract
+    )
+
+    /**
+     * Sponsors the contract as the default account
+     * @type {sponsor}
+     */
+    this.sponsor = async () => {
+        const isSponsor = this.isSponsor(web3.eth.accounts.wallet[0].address)
+        if(isSponsor)
+            throw new Error('Address is already a sponsor for the contract')
+        return sponsor()
+    }
+
+    /**
+     * Unsponsors if the default account is a sponsor for the contract.
+     * @type {sponsor}
+     */
+    this.unsponsor = async () => {
+        const isSponsor = this.isSponsor(web3.eth.accounts.wallet[0].address)
+        if(!isSponsor)
+            throw new Error('Address is not a sponsor for the contract')
+        return unsponsor()
+    }
+
+    /**
+     * Returns whether an address is a sponsor for the contract
+     */
+    this.isSponsor = address => isSponsor(address)
+
+    /**
+     * Selects a sponsor for the contract. Callable by a contract master
+     * @param sponsor
+     * @returns {*}
+     */
+    this.selectSponsor = async sponsor => {
+        let currentSponsor = await this.currentSponsor()
+        if(currentSponsor === sponsor)
+            throw new Error(`Sponsor ${sponsor} is already the current sponsor for the contract`)
+        return selectSponsor(sponsor)
+    }
+
+    /**
+     * Selects a new master for the contract. Callable by the current contract master.
+     * @param master
+     * @returns {*}
+     */
+    this.selectMaster = async master => {
+        let currentMaster = await this.currentMaster()
+        if(currentMaster === master)
+            throw new Error(`Master ${master} is already the current master for the contract`)
+        return selectMaster(master)
+    }
+
+    /**
+     * Returns the current sponsor for the contract.
+     * @type {currentSponsor}
+     */
+    this.currentSponsor = currentSponsor
+
+    /**
+     * Returns the current master for the contract.
+     * @type {master}
+     */
+    this.currentMaster = master
+
+    /**
+     * Whitelists a provided address in the contract.
+     * @param address
+     */
+    this.addUser = async address => {
+        const _isUser = await isUser(address)
+        if(!_isUser) {
+            await addUser(address)
+            console.log(`Added address to whitelist: ${address}`)
+        } else
+            console.log(`Address ${address} has already been added to the contract whitelist`)
+    }
+
+    /**
+     * Returns whether an address is included in the contract whitelist
+     * @param address
+     * @returns {*}
+     */
+    this.isUser = address => isUser(address)
+
+    /**
+     * Removes an address from the contracts' whitelist.
+     * @param address
+     * @returns {*}
+     */
+    this.removeUser = async address => {
+        const isUser = isUser(address)
+        if(!isUser)
+            throw new Error(`${address} is not a registered address on the contract whitelist`)
+        return removeUser(address)
+    }
+
+    /**
+     * Returns the current credit plan for the contract
+     */
+    this.getCreditPlan = creditPlan
+
+    /**
+     * Sets a new credit plan for the contract.
+     * Callable by the master of the contract.
+     * @param credit the maximum amount of VTHO (in wei) that can be accumulated
+     * @param recoveryRate amount of VTHO (in wei) accumulated per block to pay for transactions for each user
+     * @returns {*}
+     */
+    this.setCreditPlan = (
+        credit,
+        recoveryRate
+    ) => setCreditPlan(
+        credit,
+        recoveryRate
+    )
+
+}
+
+module.exports = {
+    MPP,
+    web3
+}
